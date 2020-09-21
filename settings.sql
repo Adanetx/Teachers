87accbe747867125f11ca08c83b23f2fe1f0f4bb
Adanetx <Adane1301992@gmail.com>
2020-09-21 01:06:58 -0500

    update sql

--- settings.sql
+++ settings.sql
@@ -1,3 +1,3 @@
 DROP DATABASE IF EXISTS "Teachers_list";
 CREATE DATABASE "Teachers_list";
-SECRET=-g2$63xely@t!tlc!rbq107v7hi*ua(nlq!*a39%f=gtjunl0v
+SECRET=")ezdlbezug=i1x6@&6-ap&yd1pf$d9v$e3=9neo&nrstex8kgc"
